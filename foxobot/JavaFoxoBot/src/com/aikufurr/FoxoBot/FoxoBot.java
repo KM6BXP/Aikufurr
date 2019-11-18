5a0a2786ba4cce200cdc11f7668c01afb0222bdb
Aikufurr <51518624+Aikufurr@users.noreply.github.com>
2019-11-18 20:35:59 +0000

    Update FoxoBot.java

--- foxobot/JavaFoxoBot/src/com/aikufurr/FoxoBot/FoxoBot.java
+++ foxobot/JavaFoxoBot/src/com/aikufurr/FoxoBot/FoxoBot.java
@@ -8,16 +8,16 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 
 public class FoxoBot {
-	public static JDA jda;
+    public static JDA jda;
 
-	// Start
-	public static void main(String[] args) throws LoginException, InterruptedException {
-		jda = new JDABuilder(AccountType.BOT).setToken("")
-				.build();
-		jda.getPresence().setActivity(Activity.watching("Foxes"));
-		jda.addEventListener(new Commands());
-		jda.addEventListener(new GenericMessageReactionAdd());
-		jda.awaitReady();
-		System.out.println("Logged in as: " + jda.getSelfUser().getName());
-	}
+    // Start
+    public static void main(String[] args) throws LoginException, InterruptedException {
+        jda = new JDABuilder(AccountType.BOT).setToken("")
+                .build();
+        jda.getPresence().setActivity(Activity.watching("Foxes"));
+        jda.addEventListener(new Commands());
+        jda.addEventListener(new GenericMessageReactionAdd());
+        jda.awaitReady();
+        System.out.println("Logged in as: " + jda.getSelfUser().getName());
+    }
 }
